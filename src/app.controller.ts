c01985633afd93203f20c29420202bd518480dc3
littwa <littwa777@gmail.com>
2021-06-16 20:40:34 +0300

    init mongo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,68 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Next, Param, Patch, Post, Put, Redirect, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response, } from 'express';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './users/user.schema';
+import { Model } from 'mongoose';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  constructor(private readonly appService: AppService, @InjectModel(User.name) private catModel: Model<UserDocument>) { }
+
+  async create(createUserDto: any): Promise<User> {
+    const createdCat = new this.catModel(createUserDto);
+    return createdCat.save();
+  }
+
+  @Post()
+  postTest(@Body() body): any {
+    this.create({
+      name: "sds",
+      age: "number",
+      breed: "fgf"
+    })
+    return body.q + 3;
+  }
+
+  @Get()
+  @Redirect('https://google.com')
+  getRedirectOneTest() {
+    return 'Redirect-google';
+  }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'getHello';
+  }
+
+  @Get(':id')
+  getOneTestRestuct(@Param('id') id): string {
+    return 'getOneTestRestuct ' + id;
+  }
+
+  @Get(':id')
+  getOneTest(@Param() params): string {
+    return 'getOneTest' + params.id;
+  }
+
+
+
+  @Put()
+  @HttpCode(HttpStatus.CREATED)
+  @Header("AnyHeader", "any_header_text")
+  putOneTest() {
+    return 'HttpStatusTest';
+  }
+
+  @Patch()
+  testNativeExpress(@Res() res, @Req() req) {
+    res.status(201).send({ q: 9 });
+  }
+
+  @Delete()
+  deleteNativeExpress(@Res() res: Response, @Req() req: Request) {
+    res.status(200).send('deleteNativeExpress')
+    // return 'deleteNativeExpress'// will not work
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { User, UserSchema } from './users/user.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://adminik:7891ly1987@cluster-mongodb.mqnmf.mongodb.net/db-bravo-nest?retryWrites=true&w=majority'), MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// mongodb+srv://adminik:7891ly1987@cluster-mongodb.mqnmf.mongodb.net/db-bravo?retryWrites=true&w=majority
+// mongodb://localhost/nest
